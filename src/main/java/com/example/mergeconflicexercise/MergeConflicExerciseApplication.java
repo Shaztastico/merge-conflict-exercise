fc4acc416033e28e0bdb227d1627c007642f21ad
August Skipper <ausk0001@stud.ek.dk>
2026-02-17 14:31:37 +0100

    Initial commit

---
+++ src/main/java/com/example/mergeconflicexercise/MergeConflicExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mergeconflicexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeConflicExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeConflicExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mergeconflicexercise/MergeConflicExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mergeconflicexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeConflicExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
